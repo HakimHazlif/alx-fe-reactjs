2d088cc54c19aa93cf56791fe4b89a43d3469e03
Hakim-56 <hakimhazlif@gmail.com>
2025-02-20 17:36:33 +0100

    Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,15 +7,15 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import "./App.css";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>

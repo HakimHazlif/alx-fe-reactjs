04a6a166f703ec208af6d1da75ae1c4c08cef956
Hakim-56 <hakimhazlif@gmail.com>
2025-03-22 05:08:39 +0000

    Add UserProfile in App component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,10 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
     <div className="bg-red-500">
       <h1 className="text-6xl">Hello World</h1>
+      <UserProfile />
     </div>
   );
 }

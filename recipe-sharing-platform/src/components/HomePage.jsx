ed5aa7dced5771bbba0a47fab15dd503899e0a44
Hakim-56 <hakimhazlif@gmail.com>
2025-03-22 06:48:32 +0000

    Put a JSX code's RecipeCard directly in HomePage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -29,7 +29,28 @@ function HomePage() {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
+          <div
+            key={recipe.id}
+            className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-green-600 text-white transition transform hover:scale-105 hover:shadow-2xl"
+          >
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="w-full h-48 object-cover"
+            />
+
+            <div className="p-4">
+              <h2 className="text-2xl font-semibold text-amber-400">
+                {recipe.title}
+              </h2>
+
+              <p className="mt-2 text-lg text-amber-200">{recipe.summary}</p>
+
+              <button className="mt-4 px-4 py-2 bg-amber-200 text-green-700 font-bold rounded-full hover:bg-amber-300 transition">
+                Read Recipe
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>

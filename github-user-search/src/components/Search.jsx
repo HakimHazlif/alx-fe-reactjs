ee50b4a78d13cdac71beaeab32815b15578252ec
Hakim-56 <hakimhazlif@gmail.com>
2025-03-24 04:31:01 +0000

    Replace can't by cant in error message

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
       setUserData(data);
       setLoading(false);
     } catch {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     }
   }
 
